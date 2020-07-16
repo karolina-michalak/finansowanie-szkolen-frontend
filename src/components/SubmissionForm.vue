43ba2a94162711d0eb2d39d7f2e46c722798f8d6
Karolina Michalak <karolina0michalak@gmail.com>
2020-07-16 15:25:19 +0200

    Created SubmissionForm component

---
+++ src/components/SubmissionForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>działa</div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default Vue.extend ({
+    name: "SubmissionForm",
+
+    data: () => ({
+        //
+    }),
+    
+})
+</script>
